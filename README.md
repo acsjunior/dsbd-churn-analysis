d98c34fc5fc8799e3e48404aefeadb8cd3a2a2c7
juniorssz <juniorssz@gmail.com>
2020-09-04 12:08:15 -0300

    updates

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# dsbd-churn-analysis
+# Customer Churn Classification Using a Logistic Regression Model
+
+<hr>
+
+###  Final paper of the Specialization Course in Data Science and Big Data of the Federal University of Paraná (UFPR)
+
+<br>
+<br>
+
+#### Abstract
+
+Developing strategies for customer retention became a common practice among companies from different segments, since long-term relationships with customers are associated to the economic survival and success of companies. Therefore, with the goal of predicting the customer churn of a Brazilian startup, this article presents a predictive model that classifies the customer churn and allows interpreting the reasons that impact the outcome. After an extensive data wrangling process, the logistic regression was applied using K-fold cross-validation and the stepwise algorithm for covariate selection. The final model, composed by 14 covariates, has undergone a diagnostic analysis through randomized quantile residuals and its prediction power was evaluated by the ROC curve, confusion matrix and evaluating metrics. The model was considered appropriate for the business in all stages of the study and not only proved to have a good prediction power, but also demonstrated to be capable of providing insights for executing optimized and customized marketing actions focusing on the retention of customers likely to churn.
+
+<b>Keywords: </b>Customer churn; Customer retention; CRM; Logistic regression; Stepwise.
+
+<hr>
+
+<b>Full paper: </b> https://github.com/juniorssz/dsbd-churn-analysis/blob/master/tex/article/article.pdf
+
+
+
+
--- scripts/train_model.R
+++ scripts/train_model.R
@@ -1,12 +1,3 @@
-#-------------------------------------------------------------------------------------------------
-# Script para ajuste do modelo de regressão logística
-#
-#
-#                                                                        Antonio C. da Silva Júnior 
-#                                                                               juniorssz@gmail.com
-#                                                                23-Jul-2020 | Curitiba/PR - Brasil
-#--------------------------------------------------------------------------------------------------
-# Requisitos:
 require(tidyverse)
 require(caret)
 require(hnp)
@@ -21,8 +12,8 @@ SEED <- 1111
 
 #--------------------------------------------------------------------------------------------------
 # Carregando os dados
-df_train <- read.csv("Data/anonymous_train.csv")
-df_test <- read.csv("Data/anonymous_test.csv")
+df_train <- read.csv("data/anonymous_train.csv")
+df_test <- read.csv("data/anonymous_test.csv")
 
 #--------------------------------------------------------------------------------------------------
 # Stepwise:
@@ -92,81 +83,5 @@ coords(roc_curve, x = cutoff, ret = c("sensitivity", "specificity", "accuracy"),
 pred <- as.factor(ifelse(pred > 0.53, 1, 0))
 confusionMatrix(pred, as.factor(df_test$y), positive = "1")
 
-
-#--------------------------------------------------------------------------------------------------
-# LASSO:
-set.seed(SEED)
-
-X_train <- model.matrix(y ~ ., df_train)
-y_train <- df_train$y
-
-X_test <- model.matrix(y ~ ., df_test)
-y_test <- df_test$y
-
-fit_lasso <- glmnet(X_train, y_train, family = 'binomial', alpha = 1, standardize = FALSE)
-plot(fit_lasso, xvar="lambda", label=TRUE, lwd = 2, cex = 20)
-
-cv_fit <- cv.glmnet(X_train, y_train, family = 'binomial', alpha = 1, nfolds = 5, grouped = F)
-plot(cv_fit)
-
-# best_lambda <- cv_fit$lambda.min
-# fit_lasso_best <- glmnet(X_train, y_train, alpha = 1, lambda = best_lambda)
-# pred <- predict(lasso_best, s = best_lam, newx = x_vars[x_test,])
-
-coefs_lasso <- coef(fit_lasso, s = cv_fit$lambda.min)
-coefs_lasso_names <- row.names(coefs_lasso)
-coefs_lasso <- as.data.frame(matrix(coefs_lasso))
-coefs_lasso$names <- coefs_lasso_names
-coefs_lasso <- coefs_lasso %>%
-  filter(V1 != 0)
-
-features <- coefs_lasso$names[coefs_lasso$names != "(Intercept)"]
-X_train <- df_train[features]
-y_train <- df_train$y
-
-set.seed(SEED)
-fit_lasso_cv <- train(x = X_train,
-                         y = y_train,
-                         trControl = trainControl(method = "cv", number = 5),
-                         method = "glm",
-                         family = "binomial")
-
-summary(fit_lasso_cv)
-logit_diag(fit_lasso_cv$finalModel)
-fit_lasso_final <- fit_lasso_cv$finalModel
-
-# desempenho
-pred <- predict(fit_lasso_final, newdata = df_test, type = "response")
-roc_curve <- roc(df_test$y, pred, plot=F, ci=T, ci.sp = T)
-auc <- round(auc(roc_curve),4)
-plot(roc_curve, print.thres = c(0.001, 0.03, 0.1, 0.2, 0.4, 0.5, 0.6, 0.62, 0.7, 0.9, 0.95, 0.96, 0.999),
-     print.thres.pattern.cex = 0.9, main = paste("AUC:", auc))
-
-prevalence <- prop.table(table(rbind(df_train, df_test)$y))[2] 
-cutoff <- as.numeric(coords(roc_curve, x = "best", best.method = "youden", 
-                            best.weights=c(1.1, prevalence), transpose = F)[1])
-print(paste("Cutoff:", cutoff))
-coords(roc_curve, x = cutoff, ret = c("sensitivity", "specificity", "accuracy"), transpose = F)
-pred <- as.factor(ifelse(pred > 0.53, 1, 0))
-confusionMatrix(pred, as.factor(df_test$y), positive = "1")
-
-
-#--------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-set.seed(SEED)
-
-model <- glm(formula = y ~ .,
-             data = df_train,
-             family = binomial)
 
 
